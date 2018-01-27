a2688213117ecfb3719080cdf54808d05ec0ba41
Shawn DeArmond <sgdearmond@ucdavis.edu>
2018-01-27 07:44:13 -0800

    Added code block from https://pantheon.io/docs/domains/#redirect-to-https-and-the-primary-domain to redirect to HTTPS.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -64,10 +64,32 @@ if (defined('PANTHEON_ENVIRONMENT')) {
     $settings['cache']['bins']['form']      = 'cache.backend.database';
 }
 
-// Trusted Host Patterns according to https://www.drupal.org/node/1992030
+if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
+    // Redirect to https://$primary_domain in the Live environment
+    if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
+        /** Replace www.example.com with your registered domain name */
+        $primary_domain = 'uccsc.ucdavis.edu';
+    }
+    else {
+        // Redirect to HTTPS on every Pantheon environment.
+        $primary_domain = $_SERVER['HTTP_HOST'];
+    }
 
-$settings['trusted_host_patterns'] = [
-    '^uccsc\.ucdavis\.edu',
-    '^.+\-uccsc\.pantheonsite\.io',
-    '^.+\.local',
-];
+    if ($_SERVER['HTTP_HOST'] != $primary_domain
+        || !isset($_SERVER['HTTP_USER_AGENT_HTTPS'])
+        || $_SERVER['HTTP_USER_AGENT_HTTPS'] != 'ON' ) {
+
+        # Name transaction "redirect" in New Relic for improved reporting (optional)
+        if (extension_loaded('newrelic')) {
+            newrelic_name_transaction("redirect");
+        }
+
+        header('HTTP/1.0 301 Moved Permanently');
+        header('Location: https://'. $primary_domain . $_SERVER['REQUEST_URI']);
+        exit();
+    }
+    // Drupal 8 Trusted Host Settings
+    if (is_array($settings)) {
+        $settings['trusted_host_patterns'] = array('^'. preg_quote($primary_domain) .'$');
+    }
+}
